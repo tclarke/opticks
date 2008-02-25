b41c192033e8ef55ee8be4bd5567a92f28008acf
tclarke <tclarke@6d0fddd1-9640-0410-91f9-aa98d5e64d34>
2008-02-25 19:20:42 +0000

    TRC85: Now we download the correct version of ossim.
    git-svn-id: http://opticks.svn.sourceforge.net/svnroot/opticks/trunk/current@1456 6d0fddd1-9640-0410-91f9-aa98d5e64d34

--- Dependencies/ossim/__init__.py
+++ Dependencies/ossim/__init__.py
@@ -1,3 +1,3 @@
 __doc__ = """OSSIM library."""
-package = {'url':"https://opticks.ballforge.net/files/documents/8/67/ossim.zip",
-           'downloadLocation':'ossim-10682-Nov14-2007.zip'}
+package = {'url':"https://opticks.ballforge.net/files/documents/8/74/ossim.zip",
+           'downloadLocation':'ossim-10682-Nov28-2007.zip'}
