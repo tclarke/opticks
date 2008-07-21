27018feb979a5b34e75d5e39c8f7aadd3cf208bb
mconsidi <mconsidi@6d0fddd1-9640-0410-91f9-aa98d5e64d34>
2008-07-21 14:05:25 +0000

    OPTICKS-161: Auto-save not working
    git-svn-id: http://opticks.svn.sourceforge.net/svnroot/opticks/trunk/4.2.X@3452 6d0fddd1-9640-0410-91f9-aa98d5e64d34

--- Code/application/Framework/InteractiveApplication.cpp
+++ Code/application/Framework/InteractiveApplication.cpp
@@ -28,6 +28,7 @@
 #include "ProgressDlg.h"
 #include "RasterLayer.h"
 #include "SessionManager.h"
+#include "SessionResource.h"
 #include "SplashScreen.h"
 #include "WizardUtilities.h"
 
@@ -97,221 +98,218 @@ int InteractiveApplication::run(int argc, char** argv)
       }
    }
 
-   // Temporarily Disable AutoSave. Use setSessionSetting so that
-   // the "real" setting is not overwritten in case of a later error
-   pConfigSettings->setSessionSetting(SessionManager::getSettingAutoSaveEnabledKey(), false);
+   { // scope the lifetime of the lock
+      SessionSaveLock lock;
 
-   // Create a progress object
-   ProgressAdapter* pProgress = new ProgressAdapter();
+      // Create a progress object
+      ProgressAdapter* pProgress = new ProgressAdapter();
 
-   // Splash screen
-   Q_INIT_RESOURCE(Application);
-   const vector<PlugInBranding>& branding = PlugInBranding::getBrandings();
-   list<string> splashImages;
-   for (vector<PlugInBranding>::const_iterator brandingIter = branding.begin();
-        brandingIter != branding.end();
-        ++brandingIter)
-   {
-      const Filename* pFilename = brandingIter->getSplashScreenImage();
-      if (pFilename != NULL)
+      // Splash screen
+      Q_INIT_RESOURCE(Application);
+      const vector<PlugInBranding>& branding = PlugInBranding::getBrandings();
+      list<string> splashImages;
+      for (vector<PlugInBranding>::const_iterator brandingIter = branding.begin();
+           brandingIter != branding.end();
+           ++brandingIter)
       {
-         string fullPathAndName = pFilename->getFullPathAndName();
-         if (!fullPathAndName.empty())
+         const Filename* pFilename = brandingIter->getSplashScreenImage();
+         if (pFilename != NULL)
          {
-            splashImages.push_back(fullPathAndName);
-         }
-      }
-   }
-   SplashScreen* pSplash = new SplashScreen(pProgress);
-   pSplash->setSplashImages(splashImages);
-   pSplash->show();
-
-   qApplication.processEvents();
-
-   // Initialization
-   pProgress->updateProgress("Building the plug-in list...", 0, NORMAL);
-
-   int iReturn = Application::run(argc, argv);
-   if (iReturn == -1)
-   {
-      pSplash->close();
-      delete pSplash;
-      return -1;
-   }
-
-   qApplication.processEvents();
-
-   // Create the main GUI window
-   pProgress->updateProgress("Creating the main application window...", 0, NORMAL);
-
-   ApplicationWindow* pAppWindow = new ApplicationWindow(pSplash);
-   qApplication.processEvents();
-
-   // Execute startup plug-ins
-   PlugInManagerServicesImp* pManager = PlugInManagerServicesImp::instance();
-   if (pManager != NULL)
-   {
-      pManager->executeStartupPlugIns(pProgress);
-      qApplication.processEvents();
-   }
-
-   // Restore the previous position and visibility state of the toolbars and dock windows
-   pAppWindow->restoreConfiguration();
-
-   // Keep the splash screen up until all images have been shown to the user.
-   while (!pSplash->canClose()) {}
-
-   // Display the main application window
-   pAppWindow->show();
-
-   // Destroy the splash screen
-   pSplash->close();
-   delete pSplash;
-
-   // Create a progress dialog
-   ProgressDlg* pProgressDlg = new ProgressDlg(APP_NAME, pAppWindow);
-   pProgress->attach(SIGNAL_NAME(Subject, Modified), Slot(pProgressDlg, &ProgressDlg::progressUpdated));
-   pProgress->attach(SIGNAL_NAME(Subject, Deleted), Slot(pProgressDlg, &ProgressDlg::progressDeleted));
-
-   // Load files specified on the command line
-   ArgumentList *pArgList(ArgumentList::instance());
-   if(pArgList != NULL)
-   {
-      bool validImport = true;
-
-      vector<string> filenames(pArgList->getOptions(""));
-      if (filenames.size() > 1)
-      {
-         bool wizardFiles = false;
-         bool datasetFiles = false;
-
-         for (vector<string>::size_type i = 0; i < filenames.size(); ++i)
-         {
-            FilenameImp filename(filenames[i]);
-
-            QString strFilename = QString::fromStdString(filename.getFullPathAndName());
-            if (strFilename.isEmpty() == false)
+            string fullPathAndName = pFilename->getFullPathAndName();
+            if (!fullPathAndName.empty())
             {
-               QFileInfo info(strFilename);
-               if ((info.suffix() == "wiz") || (info.suffix() == "batchwiz"))
-               {
-                  if (datasetFiles == true)
-                  {
-                     validImport = false;
-                     break;
-                  }
-
-                  wizardFiles = true;
-               }
-               else if (info.suffix() == "session")
-               {
-                  validImport = false;
-                  break;
-               }
-               else
-               {
-                  if (wizardFiles == true)
-                  {
-                     validImport = false;
-                     break;
-                  }
-
-                  datasetFiles = true;
-               }
+               splashImages.push_back(fullPathAndName);
             }
          }
       }
+      SplashScreen* pSplash = new SplashScreen(pProgress);
+      pSplash->setSplashImages(splashImages);
+      pSplash->show();
 
-      if (validImport == true)
+      qApplication.processEvents();
+
+      // Initialization
+      pProgress->updateProgress("Building the plug-in list...", 0, NORMAL);
+
+      int iReturn = Application::run(argc, argv);
+      if (iReturn == -1)
       {
-         for (vector<string>::size_type i = 0; i < filenames.size(); ++i)
-         {
-            FilenameImp filename(filenames[i]);
-            string normalizedFilename = filename.getFullPathAndName();
+         pSplash->close();
+         delete pSplash;
+         return -1;
+      }
 
-            QString strFilename = QString::fromStdString(normalizedFilename);
-            if (strFilename.isEmpty() == false)
+      qApplication.processEvents();
+
+      // Create the main GUI window
+      pProgress->updateProgress("Creating the main application window...", 0, NORMAL);
+
+      ApplicationWindow* pAppWindow = new ApplicationWindow(pSplash);
+      qApplication.processEvents();
+
+      // Execute startup plug-ins
+      PlugInManagerServicesImp* pManager = PlugInManagerServicesImp::instance();
+      if (pManager != NULL)
+      {
+         pManager->executeStartupPlugIns(pProgress);
+         qApplication.processEvents();
+      }
+
+      // Restore the previous position and visibility state of the toolbars and dock windows
+      pAppWindow->restoreConfiguration();
+
+      // Keep the splash screen up until all images have been shown to the user.
+      while (!pSplash->canClose()) {}
+
+      // Display the main application window
+      pAppWindow->show();
+
+      // Destroy the splash screen
+      pSplash->close();
+      delete pSplash;
+
+      // Create a progress dialog
+      ProgressDlg* pProgressDlg = new ProgressDlg(APP_NAME, pAppWindow);
+      pProgress->attach(SIGNAL_NAME(Subject, Modified), Slot(pProgressDlg, &ProgressDlg::progressUpdated));
+      pProgress->attach(SIGNAL_NAME(Subject, Deleted), Slot(pProgressDlg, &ProgressDlg::progressDeleted));
+
+      // Load files specified on the command line
+      ArgumentList *pArgList(ArgumentList::instance());
+      if(pArgList != NULL)
+      {
+         bool validImport = true;
+
+         vector<string> filenames(pArgList->getOptions(""));
+         if (filenames.size() > 1)
+         {
+            bool wizardFiles = false;
+            bool datasetFiles = false;
+
+            for (vector<string>::size_type i = 0; i < filenames.size(); ++i)
             {
-               QFileInfo info(strFilename);
-               if ((info.isFile() == true) && (info.exists() == true))
+               FilenameImp filename(filenames[i]);
+
+               QString strFilename = QString::fromStdString(filename.getFullPathAndName());
+               if (strFilename.isEmpty() == false)
                {
-                  if (info.suffix() == "wiz")
+                  QFileInfo info(strFilename);
+                  if ((info.suffix() == "wiz") || (info.suffix() == "batchwiz"))
                   {
-                     pAppWindow->runWizard(strFilename);
-                  }
-                  else if (info.suffix() == "batchwiz")
-                  {
-                     vector<string> batchFiles;
-                     batchFiles.push_back(normalizedFilename);
-                     WizardUtilities::runBatchFiles(batchFiles, pProgress);
+                     if (datasetFiles == true)
+                     {
+                        validImport = false;
+                        break;
+                     }
+
+                     wizardFiles = true;
                   }
                   else if (info.suffix() == "session")
                   {
-                     string saveKey = SessionManager::getSettingQueryForSaveKey();
-                     SessionSaveType saveType = SESSION_DONT_AUTO_SAVE;
-
-                     pConfigSettings->setSessionSetting(saveKey, saveType);
-                     pAppWindow->openSession(strFilename);
-                     pConfigSettings->deleteSessionSetting(saveKey);
+                     validImport = false;
+                     break;
                   }
                   else
                   {
-                     ImporterResource importer("Auto Importer", normalizedFilename, pProgress, false);
-                     importer->execute();
+                     if (wizardFiles == true)
+                     {
+                        validImport = false;
+                        break;
+                     }
+
+                     datasetFiles = true;
                   }
-               }
-               else
-               {
-                  reportWarning("The specified file '" + normalizedFilename +
-                     "' does not exist and cannot be loaded.");
                }
             }
          }
+
+         if (validImport == true)
+         {
+            for (vector<string>::size_type i = 0; i < filenames.size(); ++i)
+            {
+               FilenameImp filename(filenames[i]);
+               string normalizedFilename = filename.getFullPathAndName();
+
+               QString strFilename = QString::fromStdString(normalizedFilename);
+               if (strFilename.isEmpty() == false)
+               {
+                  QFileInfo info(strFilename);
+                  if ((info.isFile() == true) && (info.exists() == true))
+                  {
+                     if (info.suffix() == "wiz")
+                     {
+                        pAppWindow->runWizard(strFilename);
+                     }
+                     else if (info.suffix() == "batchwiz")
+                     {
+                        vector<string> batchFiles;
+                        batchFiles.push_back(normalizedFilename);
+                        WizardUtilities::runBatchFiles(batchFiles, pProgress);
+                     }
+                     else if (info.suffix() == "session")
+                     {
+                        string saveKey = SessionManager::getSettingQueryForSaveKey();
+                        SessionSaveType saveType = SESSION_DONT_AUTO_SAVE;
+
+                        pConfigSettings->setSessionSetting(saveKey, saveType);
+                        pAppWindow->openSession(strFilename);
+                        pConfigSettings->deleteSessionSetting(saveKey);
+                     }
+                     else
+                     {
+                        ImporterResource importer("Auto Importer", normalizedFilename, pProgress, false);
+                        importer->execute();
+                     }
+                  }
+                  else
+                  {
+                     reportWarning("The specified file '" + normalizedFilename +
+                        "' does not exist and cannot be loaded.");
+                  }
+               }
+            }
+         }
+         else if (pProgress != NULL)
+         {
+            string msg = "Unable to import the files specified on the command line.  " + string(APP_NAME) +
+               " supports loading one session file, one or more wizard files, or one or more data set files.";
+            pProgress->updateProgress(msg, 0, ERRORS);
+         }
       }
-      else if (pProgress != NULL)
+
+      // If there are any wizards, run them
+      executeStartupBatchWizards(pProgress);
+
+      // Destroy the progress object and progress dialog
+      pProgress->detach(SIGNAL_NAME(Subject, Modified), Slot(pProgressDlg, &ProgressDlg::progressUpdated));
+      pProgress->detach(SIGNAL_NAME(Subject, Deleted), Slot(pProgressDlg, &ProgressDlg::progressDeleted));
+      delete pProgress;
+
+      vector<string> autoExitOptions = pArgList->getOptions("autoExit");
+      if (autoExitOptions.empty() == false)
       {
-         string msg = "Unable to import the files specified on the command line.  " + string(APP_NAME) +
-            " supports loading one session file, one or more wizard files, or one or more data set files.";
-         pProgress->updateProgress(msg, 0, ERRORS);
+         SessionSaveType tempSettingQueryForSave = SESSION_DONT_AUTO_SAVE;
+         const string sessionFilename = autoExitOptions.front();
+         if (sessionFilename.empty() == false)
+         {
+            tempSettingQueryForSave = SESSION_AUTO_SAVE;
+            pAppWindow->setSessionFilename(FilenameImp(sessionFilename).getFullPathAndName());
+         }
+
+         pConfigSettings->setSessionSetting(SessionManager::getSettingQueryForSaveKey(), tempSettingQueryForSave);
+         pAppWindow->close();
+         return iReturn;
       }
-   }
 
-   // If there are any wizards, run them
-   executeStartupBatchWizards(pProgress);
-
-   // Destroy the progress object and progress dialog
-   pProgress->detach(SIGNAL_NAME(Subject, Modified), Slot(pProgressDlg, &ProgressDlg::progressUpdated));
-   pProgress->detach(SIGNAL_NAME(Subject, Deleted), Slot(pProgressDlg, &ProgressDlg::progressDeleted));
-   delete pProgress;
-
-   vector<string> autoExitOptions = pArgList->getOptions("autoExit");
-   if (autoExitOptions.empty() == false)
-   {
-      SessionSaveType tempSettingQueryForSave = SESSION_DONT_AUTO_SAVE;
-      const string sessionFilename = autoExitOptions.front();
-      if (sessionFilename.empty() == false)
+      // Set the application window to auto-generate textures
+      QTimer textureGenerationTimer;
+      bool generation = RasterLayer::getSettingBackgroundTileGeneration();
+      if (generation == true)
       {
-         tempSettingQueryForSave = SESSION_AUTO_SAVE;
-         pAppWindow->setSessionFilename(FilenameImp(sessionFilename).getFullPathAndName());
+         textureGenerationTimer.setInterval(100);
+         pAppWindow->connect(&textureGenerationTimer, SIGNAL(timeout()), SLOT(pregenerateTexture()));
       }
 
-      pConfigSettings->setSessionSetting(SessionManager::getSettingQueryForSaveKey(), tempSettingQueryForSave);
-      pAppWindow->close();
-      return iReturn;
    }
-
-   // Set the application window to auto-generate textures
-   QTimer textureGenerationTimer;
-   bool generation = RasterLayer::getSettingBackgroundTileGeneration();
-   if (generation == true)
-   {
-      textureGenerationTimer.setInterval(100);
-      pAppWindow->connect(&textureGenerationTimer, SIGNAL(timeout()), SLOT(pregenerateTexture()));
-   }
-
-   // Delete the temporary autoSaveEnabled setting which was created earlier
-   pConfigSettings->deleteSessionSetting(SessionManager::getSettingAutoSaveEnabledKey());
-
    // Initiate the GUI event loop, which returns when the user exits the application
    return qApplication.exec();
 }
