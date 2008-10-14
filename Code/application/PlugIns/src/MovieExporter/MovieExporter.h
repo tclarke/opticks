c0cb20449922c34c048cd0d84f6a795cabbc2934
rforehan <rforehan@6d0fddd1-9640-0410-91f9-aa98d5e64d34>
2008-10-14 20:59:47 +0000

    OPTICKS-287: Fixed problem with MovieExporter leaving partially exported file open after user aborts the export.
    git-svn-id: http://opticks.svn.sourceforge.net/svnroot/opticks/trunk/4.2.X@4341 6d0fddd1-9640-0410-91f9-aa98d5e64d34

--- Code/application/PlugIns/src/MovieExporter/MovieExporter.h
+++ Code/application/PlugIns/src/MovieExporter/MovieExporter.h
@@ -26,6 +26,119 @@ class OptionsMovieExporter;
 class Progress;
 class QWidget;
 
+class AvFormatContextObject
+{
+public:
+   class Args
+   {
+   public:
+      AVOutputFormat* mpOutputFormat;
+      Args(AVOutputFormat* pOutputFormat = NULL) : 
+         mpOutputFormat(pOutputFormat) {}
+   };
+
+   AVFormatContext* obtainResource(const Args &args) const 
+   {
+      AVFormatContext* pFormat(NULL);
+      if (args.mpOutputFormat == NULL)
+      {
+         return pFormat;
+      }
+      pFormat = av_alloc_format_context();
+      VERIFYRV(pFormat, NULL);
+      pFormat->oformat = args.mpOutputFormat;
+      return pFormat;
+   }
+
+   void releaseResource(const Args &args, AVFormatContext *pFormat) const 
+   { 
+      if (pFormat != NULL)
+      {
+         for (int i = 0; i < pFormat->nb_streams; i++)
+         {
+            av_freep(&pFormat->streams[i]->codec);
+            av_freep(&pFormat->streams[i]);
+         }
+         url_fclose(&pFormat->pb);
+         av_free(pFormat);
+      }
+   }
+};
+
+class AvFormatContextResource : public Resource<AVFormatContext, AvFormatContextObject>
+{
+public:
+   AvFormatContextResource(AVOutputFormat* pOutputFormat) : 
+      Resource<AVFormatContext, AvFormatContextObject>(AvFormatContextObject::Args(pOutputFormat)) {}
+      operator AVFormatContext*() { return get(); }
+};
+
+class AvStreamObject
+{
+public:
+   class Args
+   {
+   public:
+      AVFormatContext* mpFormat;
+      CodecID mVideoCodec;
+      Args(AVFormatContext* pFormat = NULL, CodecID videoCodec = CODEC_ID_NONE) : 
+         mpFormat(pFormat), mVideoCodec(videoCodec) {}
+   };
+
+   AVStream* obtainResource(const Args &args) const 
+   {
+      AVStream* pVideoStream(NULL);
+      if (args.mpFormat == NULL || args.mVideoCodec == CODEC_ID_NONE)
+      {
+         return pVideoStream;
+      }
+      pVideoStream = av_new_stream(args.mpFormat, 0);
+      VERIFYRV(pVideoStream, NULL);
+
+      AVCodecContext *pCodecContext = pVideoStream->codec;
+      pCodecContext->codec_id = args.mVideoCodec;
+      pCodecContext->codec_type = CODEC_TYPE_VIDEO;
+
+      pCodecContext->gop_size = 12; /* emit one intra frame every twelve frames at most */
+      pCodecContext->pix_fmt = PIX_FMT_YUV420P;
+      if (args.mpFormat->oformat->flags & AVFMT_GLOBALHEADER)
+      {
+         pCodecContext->flags |= CODEC_FLAG_GLOBAL_HEADER;
+      }
+      if (pCodecContext->codec_id == CODEC_ID_MPEG1VIDEO)
+      {
+         /* needed to avoid using macroblocks in which some coeffs overflow
+         this doesn't happen with normal video, it just happens here as the
+         motion of the chroma plane doesn't match the luma plane */
+         pCodecContext->mb_decision=2;
+      }
+      // some formats want stream headers to be separate
+      std::string format = args.mpFormat->oformat->name;
+      if (format == "mp4" || format == "mov" || format == "3gp")
+      {
+         pCodecContext->flags |= CODEC_FLAG_GLOBAL_HEADER;
+      }
+
+      return pVideoStream;
+   }
+
+   void releaseResource(const Args &args, AVStream *pStream) const 
+   { 
+      if (pStream != NULL)
+      {
+         avcodec_close(pStream->codec); 
+      }
+   }
+};
+
+class AvStreamResource : public Resource<AVStream, AvStreamObject>
+{
+public:
+   AvStreamResource(AVFormatContext *pFormat = NULL, CodecID videoCodec = CODEC_ID_NONE) : 
+      Resource<AVStream, AvStreamObject>(AvStreamObject::Args(pFormat, videoCodec)) {}
+   operator AVStream*() { return get(); }
+};
+
 /**
  *  Movie Exporter
  *
@@ -45,11 +158,9 @@ public:
 
 protected:
    bool setAvCodecOptions(AVCodecContext *pContext, PlugInArgList *pInArgList);
-   AVStream *add_video_stream(AVFormatContext *pFormatContext, CodecID codec_id);
    bool open_video(AVFormatContext *pFormat, AVStream *pVideoStream);
    AVFrame *alloc_picture(int pixFmt, int width, int height);
    bool write_video_frame(AVFormatContext *pFormat, AVStream *pVideoStream);
-   void close_video(AVFormatContext *pFormat, AVStream *pVideoStream);
 
    virtual AVOutputFormat *getOutputFormat() const = 0;
 
