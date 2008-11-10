8862b589543cfcb249e1f90e846d8142eda1b7e0
tclarke <tclarke@6d0fddd1-9640-0410-91f9-aa98d5e64d34>
2008-11-10 19:34:47 +0000

    OPTICKS-224: Generic GDAL based importer
    git-svn-id: http://opticks.svn.sourceforge.net/svnroot/opticks/trunk/4.2.X@4669 6d0fddd1-9640-0410-91f9-aa98d5e64d34

---
+++ Code/application/CompileSettings/gdal.py
@@ -0,0 +1,25 @@
+import os
+import os.path
+import SCons.Warnings
+
+class GdalNotFound(SCons.Warnings.Warning):
+    pass
+SCons.Warnings.enableWarningClass(GdalNotFound)
+
+def generate(env):
+    path = os.environ.get('OPTICKSDEPENDENCIES',None)
+    gdalpath = ""
+    jpegpath = ""
+    if path:
+       gdalpath = os.path.join(path, "gdal")
+       jpegpath = os.path.join(path, "libjpeg")
+    if not gdalpath or not jpegpath:
+       SCons.Warnings.warn(GdalNotFound,"Could not detect gdal")
+    else:
+       env.AppendUnique(CXXFLAGS="-I%s/include/%s -I%s/include" % (gdalpath,env["PLATFORM"],jpegpath),
+                        LIBPATH=['%s/lib/%s' % (gdalpath,env["PLATFORM"]),
+                                 '%s/lib/%s' % (jpegpath,env["PLATFORM"])],
+                        LIBS=["gdal","jpeg"])
+
+def exists(env):
+    return env.Detect('gdal')
